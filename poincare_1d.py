62cfd556cd8f04cde7b0a1aab7245c60730355f2
Nicholas Lawrance <nicholas.lawrance@mavt.ethz.ch>
2020-02-21 20:42:55 +0100

    Exact time solution, phase plot

--- poincare_1d.py
+++ poincare_1d.py
@@ -16,65 +16,118 @@ class PoincarePlotter(object):
     x = []
     artists = []
 
-    def __init__(self, dx_dt, ax, n_samples=1, delta_t=1e-2, n_max=1000):
-        self.dx_dt = dx_dt
-        self.ax = ax
+    def __init__(self, dynamic_function, ax, x0=[0.0], delta_t=1e-2, n_max=1000, graph_max=10.0):
+        self.f = dynamic_function
+        self.ax = ax[0]
+        self.ax2 = ax[1]
         self.delta_t = delta_t
         self.n_max = n_max
+        self.x0 = np.array(x0)
+        self.graph_max = graph_max
 
-        if n_samples == 1:
-            self.x0 = [0.0]
-        else:
-            self.x0 = np.linspace(-2.0, 2.0, n_samples)
-
-        self.colours = cm.viridis(np.linspace(0, 1.0, n_samples))
+        self.colours = cm.get_cmap()(np.linspace(0, 1.0, len(self.x0)+2))[1:-1]
         self.init()
 
     def init(self):
         self.t = [0.0]
         self.x = [[x] for x in self.x0]
+        self.dxdt = [[self.f.dx_dt(x, self.t[0])] for x in self.x0]
         self.ax.cla()
-        self.ax.set_xlim(0, 1.0)
+        self.ax.set_xlim(0, 2.0)
         self.y_lim = [-1.0, 1.0]
+        self.ax2.cla()
+
         self.artists = []
-        for x, c in zip(self.x, self.colours):
-            l, = self.ax.plot(self.t, x, color=c)
-            p, = self.ax.plot(self.t, x, 'o', color=c)
-            self.artists.append(l)
-            self.artists.append(p)
-        self.ax.grid()
+        self.legend_entries = []
+        for x, dxdt, c in zip(self.x, self.dxdt, self.colours):
+            l, = self.ax.plot(self.t, x, c=c, lw=2.0)
+            p, = self.ax.plot(self.t, x, 'o', c=l.get_color())
+            l2, = self.ax2.plot(x, dxdt, c=c, lw=2.0)
+            p2, = self.ax2.plot(x[0], dxdt[0], 'o', c=l.get_color())
+            self.artists.extend([l, p, l2, p2])
+            self.legend_entries.append('$x_0={0:0.2f}$'.format(x[0]))
+        self.ax.grid(True)
+        self.ax2.grid(True)
         self.ax.set_xlabel('$t$')
         self.ax.set_ylabel('$x$')
+        self.ax.set_title(self.f.fname)
+        self.ax2.set_xlabel('$x$')
+        self.ax2.set_ylabel('$\partial x / \partial t$')
+        self.ax2.set_title(self.f.fname)
+
+        if len(self.x0) <= 5:
+            self.ax.legend(self.artists[0:-1:4], self.legend_entries, loc=1)
         return self.artists
 
     def animate(self, i):
         self.t.append(self.t[-1] + self.delta_t)
         self.t = self.t[-max(self.n_max, i):]
-        for i, x in enumerate(self.x):
-            nx = x[-1] + self.delta_t*self.dx_dt(x[-1], self.t[-2])
-            self.y_lim[0] = min(self.y_lim[0], nx)
-            self.y_lim[1] = max(self.y_lim[1], nx)
+        y_lim2 = self.ax2.get_ylim()
+        x_lim2 = self.ax2.get_xlim()
+        for i, (x, dxdt, x0) in enumerate(zip(self.x, self.dxdt, self.x0)):
+            if self.f.xt is not None:
+                nx = self.f.xt(self.t[-1], x0)
+            else:
+                nx = x[-1] + self.delta_t*dxdt[-1]
+            self.y_lim[0] = max(min(self.y_lim[0], nx-0.1), -self.graph_max)
+            self.y_lim[1] = min(max(self.y_lim[1], nx+0.1), self.graph_max)
             x.append(nx)
+            ndxdt = self.f.dx_dt(x[-1], self.t[-2])
+            dxdt.append(ndxdt)
             x = x[-max(self.n_max, i):]
+            dxdt = dxdt[-max(self.n_max, i):]
 
-            self.artists[2*i].set_data(self.t, x)                  # Lines
-            self.artists[2*i+1].set_data(self.t[-1], x[-1])        # Points
+            self.artists[4*i].set_data(np.array(self.t), x)                  # Lines
+            self.artists[4*i+1].set_data(self.t[-1], x[-1])        # Points
+            self.artists[4*i+2].set_data(x, dxdt)                  # Lines
+            self.artists[4*i+3].set_data(x[-1], dxdt[-1])        # Points
+
+            x_lim2 = [max(min(x_lim2[0], x[-1]), -self.graph_max), min(max(x_lim2[1], x[-1]), self.graph_max)]
+            y_lim2 = [max(min(y_lim2[0], dxdt[-1]), -self.graph_max), min(max(y_lim2[1], dxdt[-1]), self.graph_max)]
         self.ax.set_ylim(self.y_lim)
-        self.ax.set_xlim(0, max(1.0, self.t[-1]))
+        self.ax.set_xlim(0, max(2.0, self.t[-1]))
+        self.ax2.set_xlim(x_lim2)
+        self.ax2.set_ylim(y_lim2)
+
         return self.artists
 
 
-def linear_damper(x, t, k=2.0):
-    x_dot = -x + 2*np.cos(t*2*np.pi)
+class DynamicFunction(object):
+    def __init__(self, dx_dt, xt=None, fname=''):
+        self.dx_dt = dx_dt
+        self.xt = xt        # Exact solution for x(t) (if known)
+        self.fname = fname
+
+
+def linear_damper(x, t):
+    x_dot = - x + 2*np.cos(t)
     return x_dot
 
 
-n_samples = 11
-delta_t = 1e-3
+def linear_exact(t, x0):
+    return np.exp(-t)*(x0-1) + np.cos(t) + np.sin(t)
 
-fh, ah = plt.subplots()
 
-animator = PoincarePlotter(linear_damper, ah, n_samples=n_samples)
+def logisitic_periodic(x, t):
+    return - x*(1+x) + 2*np.cos(t)
 
-animation = FuncAnimation(fh, animator.animate, init_func=animator.init, frames=1000, interval=20)
-plt.show()
+
+linear = DynamicFunction(linear_damper, xt=linear_exact, fname='$\dot{x} = -x + 2\cos(t)$')
+logistic = DynamicFunction(logisitic_periodic, fname='$\dot{x} = -x(x+1) + 2\cos(t)$')
+
+delta_t = 0.05
+
+# x0 = [1.0]
+# x0 = [0.9, 1.0, 1.1]
+x0 = [-5.0, 0.0, 1.0, 2.0, 7.0]
+# x0 = np.linspace(-2.0, 2.0, 5)
+
+with plt.style.context('ggplot'):
+    fh, ah = plt.subplots(1, 2)
+    fh.set_size_inches([13.5, 6])
+
+    animator = PoincarePlotter(logistic, ah, x0=x0, delta_t=delta_t)
+
+    animation = FuncAnimation(fh, animator.animate, init_func=animator.init, frames=1000, interval=20)
+    # animation.save('vid/single_x0.mp4', writer='ffmpeg')
+    plt.show()
