4a6265939aea2a6e43d68826f5c74e5462721d08
Nicholas Lawrance <nicholas.lawrance@mavt.ethz.ch>
2020-02-22 21:01:04 +0100

    Refactor plot code  - Precalc full dynamics  - Nice wait bar (tqdm)  - Way faster plotting (blitting)  - Basic commenting

--- poincare_1d.py
+++ poincare_1d.py
@@ -3,20 +3,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
-from matplotlib import rc
 from matplotlib.animation import FuncAnimation
+from tqdm import tqdm
 
-
-## for Palatino and other serif fonts use:
-#rc('font',**{'family':'serif','serif':['Palatino']})
-rc('text', usetex=True)
 
 class PoincarePlotter(object):
+    # Class for plotting time evolution of one-dimensional periodic dynamic functions (technically any 1D dynamics)
     t = 0.0
     x = []
     artists = []
+    y_lim = [-1.0, 1.0]
+    legend_entries = []
 
-    def __init__(self, dynamic_function, ax, x0=[0.0], delta_t=1e-2, n_max=1000, graph_max=10.0):
+    def __init__(self, dynamic_function, ax, x0=[0.0], frames=1000, delta_t=1e-2, n_max=1000, graph_max=10.0):
+        assert isinstance(dynamic_function, DynamicFunction)
         self.f = dynamic_function
         self.ax = ax[0]
         self.ax2 = ax[1]
@@ -24,14 +24,34 @@ class PoincarePlotter(object):
         self.n_max = n_max
         self.x0 = np.array(x0)
         self.graph_max = graph_max
+        self.frames = frames
 
         self.colours = cm.get_cmap()(np.linspace(0, 1.0, len(self.x0)+2))[1:-1]
+
+        # Solve equations:
+        self.x = np.zeros((self.frames + 1, len(self.x0)), dtype=float)
+        self.dx_dt = self.x.copy()
+        self.x[0] = self.x0
+
+        self.t = np.arange(0.0, (self.frames + 1) * self.delta_t, self.delta_t)
+
+        for i in tqdm(range(self.frames + 1), desc='Solving dynamics: '):
+
+            if i is 0:
+                self.dx_dt[i] = self.f.dx_dt(self.x[i], self.t[i])
+                continue
+
+            if self.f.xt is not None:
+                self.x[i] = self.f.xt(self.t[i], self.x0)
+            else:
+                # TODO: Change to better integration scheme (RK4)
+                # Euler integration
+                self.x[i] = self.x[i-1] + self.delta_t * self.dx_dt[i-1]
+            self.dx_dt[i] = self.f.dx_dt(self.x[i], self.t[i])
+
         self.init()
 
     def init(self):
-        self.t = [0.0]
-        self.x = [[x] for x in self.x0]
-        self.dxdt = [[self.f.dx_dt(x, self.t[0])] for x in self.x0]
         self.ax.cla()
         self.ax.set_xlim(0, 2.0)
         self.y_lim = [-1.0, 1.0]
@@ -39,13 +59,13 @@ class PoincarePlotter(object):
 
         self.artists = []
         self.legend_entries = []
-        for x, dxdt, c in zip(self.x, self.dxdt, self.colours):
-            l, = self.ax.plot(self.t, x, c=c, lw=2.0)
-            p, = self.ax.plot(self.t, x, 'o', c=l.get_color())
-            l2, = self.ax2.plot(x, dxdt, c=c, lw=2.0)
-            p2, = self.ax2.plot(x[0], dxdt[0], 'o', c=l.get_color())
+        for x, dx_dt, c in zip(self.x[0], self.dx_dt[0], self.colours):
+            l, = self.ax.plot(self.t[0], x, c=c, lw=2.0)
+            p, = self.ax.plot(self.t[0], x, 'o', c=l.get_color())
+            l2, = self.ax2.plot(x, dx_dt, c=c, lw=2.0)
+            p2, = self.ax2.plot(x, dx_dt, 'o', c=l.get_color())
             self.artists.extend([l, p, l2, p2])
-            self.legend_entries.append('$x_0={0:0.2f}$'.format(x[0]))
+            self.legend_entries.append('$x_0={0:0.2f}$'.format(x))
         self.ax.grid(True)
         self.ax2.grid(True)
         self.ax.set_xlabel('$t$')
@@ -60,32 +80,25 @@ class PoincarePlotter(object):
         return self.artists
 
     def animate(self, i):
-        self.t.append(self.t[-1] + self.delta_t)
-        self.t = self.t[-max(self.n_max, i):]
+        assert i <= self.frames
+
+        for j in range(self.x.shape[1]):
+            self.artists[4*j].set_data(self.t[:i], self.x[:i, j])               # Lines
+            self.artists[4*j+1].set_data(self.t[i], self.x[i, j])               # Points
+            self.artists[4*j+2].set_data(self.x[:i, j], self.dx_dt[:i, j])       # Lines
+            self.artists[4*j+3].set_data(self.x[i, j], self.dx_dt[i, j])         # Points
+
+        self.y_lim[0] = max(min(self.y_lim[0], self.x[i].min()-0.1), -self.graph_max)
+        self.y_lim[1] = min(max(self.y_lim[1], self.x[i].max()+0.1), self.graph_max)
+        self.ax.set_ylim(self.y_lim)
+        self.ax.set_xlim(0, max(2.0, self.t[i]))
+
         y_lim2 = self.ax2.get_ylim()
         x_lim2 = self.ax2.get_xlim()
-        for i, (x, dxdt, x0) in enumerate(zip(self.x, self.dxdt, self.x0)):
-            if self.f.xt is not None:
-                nx = self.f.xt(self.t[-1], x0)
-            else:
-                nx = x[-1] + self.delta_t*dxdt[-1]
-            self.y_lim[0] = max(min(self.y_lim[0], nx-0.1), -self.graph_max)
-            self.y_lim[1] = min(max(self.y_lim[1], nx+0.1), self.graph_max)
-            x.append(nx)
-            ndxdt = self.f.dx_dt(x[-1], self.t[-2])
-            dxdt.append(ndxdt)
-            x = x[-max(self.n_max, i):]
-            dxdt = dxdt[-max(self.n_max, i):]
-
-            self.artists[4*i].set_data(np.array(self.t), x)                  # Lines
-            self.artists[4*i+1].set_data(self.t[-1], x[-1])        # Points
-            self.artists[4*i+2].set_data(x, dxdt)                  # Lines
-            self.artists[4*i+3].set_data(x[-1], dxdt[-1])        # Points
-
-            x_lim2 = [max(min(x_lim2[0], x[-1]), -self.graph_max), min(max(x_lim2[1], x[-1]), self.graph_max)]
-            y_lim2 = [max(min(y_lim2[0], dxdt[-1]), -self.graph_max), min(max(y_lim2[1], dxdt[-1]), self.graph_max)]
-        self.ax.set_ylim(self.y_lim)
-        self.ax.set_xlim(0, max(2.0, self.t[-1]))
+        x_lim2 = [max(min(x_lim2[0], self.x[i].min()), -self.graph_max),
+                  min(max(x_lim2[1], self.x[i].max()), self.graph_max)]
+        y_lim2 = [max(min(y_lim2[0], self.dx_dt[i].min()), -self.graph_max),
+                  min(max(y_lim2[1], self.dx_dt[i].max()), self.graph_max)]
         self.ax2.set_xlim(x_lim2)
         self.ax2.set_ylim(y_lim2)
 
@@ -101,11 +114,10 @@ class DynamicFunction(object):
 
 
 def linear_damper(x, t):
-    x_dot = - x + 2*np.cos(t)
-    return x_dot
+    return - x + 2*np.cos(t)
 
 
-def linear_exact(t, x0):
+def linear_exact(t, x0=0):
     return np.exp(-t)*(x0-1) + np.cos(t) + np.sin(t)
 
 
@@ -114,27 +126,29 @@ def logistic_periodic(x, t):
 
 
 if __name__ == '__main__':
+    plt.rc('text', usetex=True)
+
     linear = DynamicFunction(linear_damper, xt=linear_exact, fname='$\dot{x} = -x + 2\cos(t)$')
     logistic = DynamicFunction(logistic_periodic, fname='$\dot{x} = -x(x+1) + 2\cos(2 \pi t)$', period=1.0)
 
     delta_t = 0.015
 
-    # x0 = [1.0]
-    # x0 = [0.9, 1.0, 1.1]
-    # x0 = [-9.0, -4.0, -1.0, 0.5, 1.0, 1.5, 3.0, 11.0]
-    # x0 = [-5.0, 0.0, 1.0, 2.0, 7.0]
-    # x0 = np.linspace(-2.0, 2.0, 5)
-    # x0 = [0.0]
-    # x0 = [-1.0, 0.0, 1.0, 2.0]
-    x0 = np.linspace(-1.1, -1.0, 11)
-    # x0 = np.append(x0, [0.0, 1.0, 5.0, 10.0])
+    # x_0 = [1.0]
+    # x_0 = [0.9, 1.0, 1.1]
+    # x_0 = [-9.0, -4.0, -1.0, 0.5, 1.0, 1.5, 3.0, 11.0]
+    # x_0 = [-5.0, 0.0, 1.0, 2.0, 7.0]
+    # x_0 = np.linspace(-2.0, 2.0, 5)
+    # x_0 = [0.0]
+    # x_0 = [-1.0, 0.0, 1.0, 2.0]
+    x_0 = np.linspace(-1.1, -1.0, 11)
+    # x_0 = np.append(x_0, [0.0, 1.0, 5.0, 10.0])
 
     with plt.style.context('ggplot'):
         fh, ah = plt.subplots(1, 2)
         fh.set_size_inches([13.5, 6])
 
-        animator = PoincarePlotter(logistic, ah, x0=x0, delta_t=delta_t)
+        animator = PoincarePlotter(logistic, ah, x0=x_0, delta_t=delta_t)
 
-        animation = FuncAnimation(fh, animator.animate, init_func=animator.init, frames=1000, interval=20)
+        animation = FuncAnimation(fh, animator.animate, init_func=animator.init, frames=1000, interval=20, blit=True)
         # animation.save('vid/single_x0.mp4', writer='ffmpeg')
         plt.show()
--- poincare_map.py
+++ poincare_map.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
-import poincare_1d
+from poincare_1d import DynamicFunction, linear_damper, linear_exact, logistic_periodic
 
 rc('text', usetex=True)
 
 
 def calculate_poincare_map(dyn_func, x0, nt=100):
-
+    # Calculate the Poincar\'e map for a given dynamic function
+    # If exact solution known, solve directly. If not, Euler integration with nt steps
     if dyn_func.xt is not None:     # Have exact solution
         Px = dyn_func.xt(dyn_func.period, x0)
     else:
@@ -22,17 +23,27 @@ def calculate_poincare_map(dyn_func, x0, nt=100):
     return Px
 
 
-x_0 = np.linspace(-1.2, 2.0, 201)
+def follow_stability(x, Px, x0, n_jumps=5):
+    xx = [x0]
+    for i in range(n_jumps):
+        xx.append(np.interp(xx[-1], Px, x))
+    return xx
 
-linear = poincare_1d.DynamicFunction(poincare_1d.linear_damper, xt=poincare_1d.linear_exact, fname='$\dot{x} = -x + 2\cos(t)$')
-logistic = poincare_1d.DynamicFunction(poincare_1d.logistic_periodic, fname='$\dot{x} = -x(x+1) + 2\cos(2 \pi t)$', period=1.0)
 
-Px = calculate_poincare_map(logistic, x_0)
+x_0 = np.linspace(-3.0, 5.0, 201)
+
+linear = DynamicFunction(linear_damper, xt=linear_exact, fname='$\dot{x} = -x + 2\cos(t)$')
+logistic = DynamicFunction(logistic_periodic, fname='$\dot{x} = -x(x+1) + 2\cos(2 \pi t)$', period=1.0)
+
+test_fun = linear
+Px = calculate_poincare_map(test_fun, x_0)
 
 with plt.style.context('ggplot'):
     fh, ah = plt.subplots()
     ah.set_xlabel('$x_0$')
     ah.set_ylabel('$P(x_0)$')
-    hP = ah.plot(x_0, Px)
-    ah.plot([x_0[0], x_0[-1]], [x_0[0], x_0[-1]], '--')
+    hP, = ah.plot(x_0, Px)
+    h0, = ah.plot([x_0[0], x_0[-1]], [x_0[0], x_0[-1]], '--')
+    ah.set_title(r"Poincar\'e map for {0}".format(test_fun.fname))
+    ah.legend([hP, h0], ['$P(x_0)$', '$P(x)=x$'])
     plt.show()
